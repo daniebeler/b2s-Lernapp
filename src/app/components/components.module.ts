5027fe7e4ec14b183cbea36135f4182c6e83cd75
Emanuel <emanuel.hiebeler@gmail.com>
2021-08-24 11:35:27 +0200

    footer as custom component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CustomHeaderComponent } from './custom-header/custom-header.component';
+import { CustomFooterComponent } from './custom-footer/custom-footer.component';
 
 @NgModule ({
   imports: [IonicModule],
-  declarations: [CustomHeaderComponent],
-  exports: [CustomHeaderComponent]
+  declarations: [CustomHeaderComponent, CustomFooterComponent],
+  exports: [CustomHeaderComponent, CustomFooterComponent]
 })
 
 export class ComponentsModule{}

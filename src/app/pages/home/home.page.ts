28a6e72b5edd6dfea4cd4faa9e7bbaad910baab4
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2021-09-01 13:22:03 +0200

    alles kaputt

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { ElementSchemaRegistry } from '@angular/compiler';
 import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 import { StorageService } from 'src/app/services/storage.service';
 
@@ -12,11 +11,12 @@ import { StorageService } from 'src/app/services/storage.service';
 })
 export class HomePage {
 
-  courses: Array <string> = [];
-  quotes: Array <string> = [];
+  courses: Array<string> = [];
+  quotes: Array<string> = [];
   data: any = [];
   currentcourse: string;
   percent: string;
+  userStats: any = [];
 
   constructor(
     private httpClient: HttpClient,
@@ -25,28 +25,39 @@ export class HomePage {
     private storageService: StorageService
   ) {
     this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
-   }
-
-
-// eslint-disable-next-line @angular-eslint/use-lifecycle-interface
-ngOnInit() {
-  this.httpClient.get('./assets/data/questions.json').subscribe(data => {
-    this.data = data;
-    this.datareader();
-  });
-  this.setPercentOfProgressCircle(80);
-  this.storageService.set('test', 'fuf');
-  console.log(this.storageService.get('test'));
-}
-
-datareader() {
-  // eslint-disable-next-line @typescript-eslint/prefer-for-of
-  for (let i = 0; i < this.data.scheine.length; i++) {
-    this.courses.push(this.data.scheine[i].scheinName);
-    this.quotes.push(this.data.scheine[i].quote);
   }
 
-}
+
+  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+  ngOnInit() {
+    this.httpClient.get('./assets/data/questions.json').subscribe(data => {
+      this.data = data;
+      this.datareader();
+
+
+      this.storageService.set('test', JSON.stringify(this.data));
+      this.userStats = this.getJSON().then(res =>{
+        console.log(res);
+      });
+
+    });
+    this.setPercentOfProgressCircle(80);
+  }
+
+  async getJSON() {
+    this.storageService.getStorage('test').then(data => {
+      return JSON.parse(data);
+    });
+  }
+
+  datareader() {
+    // eslint-disable-next-line @typescript-eslint/prefer-for-of
+    for (let i = 0; i < this.data.scheine.length; i++) {
+      this.courses.push(this.data.scheine[i].scheinName);
+      this.quotes.push(this.data.scheine[i].quote);
+    }
+
+  }
 
   navigate(site: string) {
 
@@ -60,7 +71,7 @@ datareader() {
     this.router.navigate(['tabs/home/quiztypes/' + scheinZahl]);
   }
 
-  nav(){
+  nav() {
     this.router.navigate(['tabs/']);
 
   }
@@ -76,7 +87,7 @@ datareader() {
     else {
       document.documentElement.style.setProperty('--background-processCircle', '#777777');
     }
-    document.documentElement.style.setProperty('--transform', 'rotate('+deg+'deg)');
+    document.documentElement.style.setProperty('--transform', 'rotate(' + deg + 'deg)');
   }
 
 }
--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -11,17 +11,13 @@ export class StorageService {
     private storage: Storage
   ) {
     this.storage.create();
-   }
+  }
 
   async set(key: string, value: any): Promise<any> {
     await this.storage.set(key, value);
   }
 
-  async get(key: string): Promise<any> {
-    await this.storage.get(key).then((result) => {
-      console.log('My result', result);
-      return result;
-});;
+  getStorage(key: string): Promise<any> {
+    return this.storage.get(key);
   }
-
 }
